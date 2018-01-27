977aea07a90f3159eb33a90e76740a7022b6fb5c
Michael Brickler <mbrick02@yahoo.com>
2018-01-27 16:24:35 -0500

    add comment to seeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         // factory(App\User::class, 50)->create() //->each(function($u) { $u->posts()->save(factory(App\Post::class)->make());});
         // 1/27/18 Note: above is based on laravel.com my Post factory is based on:
     	// 			https://laracasts.com/series/laravel-from-scratch-2017/episodes/22
+    	// ?? run seeders separately: $this->call([ UsersTableSeeder::class, PostsTableSeeder::class,...]);
     }
 }
